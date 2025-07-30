37d82d99deeca82bb0936658f6f455d284c65237
Chackmoole <cebep-21@mail.ru>
2025-07-30 10:31:13 +0300

    добавлена карточка котов

---
+++ src/components/blocks/star-card/star-card.jsx
@@ -0,0 +1,9 @@
+import './star-card-style.css';
+
+export const StarCard = ({ src, title, text }) => {    
+    return <li className='star-card'>
+        <img className='star-card__pic' src={src} alt={src} width={313} height={313} />
+        <p className='star-card__title'>{title}</p>
+        <p className='star-card__description'>{text}</p>
+    </li>
+}
--- src/components/blocks/stars-list/stars-list.jsx
+++ src/components/blocks/stars-list/stars-list.jsx
@@ -1,6 +1,9 @@
 import './stars-list-style.css';
+import firstPic from '../../../assests/img/cat-lucky.jpg'
+import { StarCard } from '../star-card/star-card';
 
 export const StarsList = () => { 
     return <ul className='stars-list'>
+        <StarCard src={ firstPic } title='Кот Лаки' text='Крайне спокойный, ласковый малыш, который медленно, но уверенно перестаёт быть робким скромнягой'/>
     </ul>
 }
