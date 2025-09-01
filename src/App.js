fd64a4326c3b314cb4ea469095815b67889f17e9
Chackmoole <cebep-21@mail.ru>
2025-09-01 11:27:35 +0300

    Добавлена тема по умолчанию через ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Layout } from './components/layout/layout';
 import { Main } from './components/blocks/main/main';
 import './App.css';
+import { GlobalStyle } from './styles';
 
 function App() {
   return (
-    <Layout>
-      <Main />
-    </Layout>
+    <>
+      <GlobalStyle />
+      <Layout>
+        <Main />
+      </Layout>
+    </>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  height: 100%;
+}
+
+body,
+html {
+  margin: 0;
+}
+
+body {
+  position: relative;
+  min-height: 100%;
+  font-family: ${(props) => props.theme.fontFamily};
+  font-size: ${(props) => props.theme.fontSizeDefault};
+  line-height: 27px;
+  font-weight: 400;
+  color: ${(props => props.theme.colorBlackForText)};
+}
+
+.visually-hidden {
+  position: absolute;
+
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+
+  white-space: nowrap;
+
+  -webkit-clip-path: inset(100%);
+  clip-path: inset(100%);
+
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+}
+  `;
