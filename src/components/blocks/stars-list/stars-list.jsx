2c94a549fc00c57c4d934b912106ebda22d7943a
Chackmoole <cebep-21@mail.ru>
2025-09-02 16:00:28 +0300

    Layout + Stars-list Styled

--- src/components/blocks/stars-list/stars-list.jsx
+++ src/components/blocks/stars-list/stars-list.jsx
@@ -1,10 +1,20 @@
-import './stars-list-style.css';
-import { StarCard } from '../star-card/star-card';
 import { catsList } from '../../../mocks/cats-list';
+import { StarCard } from '../star-card/star-card';
+import { SyledStars } from './stars-list-styled';
 
-export const StarsList = () => {  
-
-    return <ul className='stars-list'>
-        {catsList.map((item) => <StarCard key={item.id} src={item.image} title={item.name} text={item.about} feature={item.feature} icon={ item.feature}></StarCard>)}
-    </ul>
-}
+export const StarsList = () => {
+	return (
+		<SyledStars>
+			{catsList.map((item) => (
+				<StarCard
+					key={item.id}
+					src={item.image}
+					title={item.name}
+					text={item.about}
+					feature={item.feature}
+					icon={item.feature}
+				></StarCard>
+			))}
+		</SyledStars>
+	);
+};
---
+++ src/components/layout/layout-styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledLayout = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 1280px;
+	margin: 0 auto;
+	min-height: 100vh;
+`;
