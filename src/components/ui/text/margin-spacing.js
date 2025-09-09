d579cdb4e5509eca30ec887d85ffe05b29ec3da4
Chackmoole <cebep-21@mail.ru>
2025-09-09 16:47:53 +0300

    fix log

--- src/components/ui/text/margin-spacing.js
+++ src/components/ui/text/margin-spacing.js
@@ -2,45 +2,35 @@ import { css } from 'styled-components';
 
 export const marginSpacing = css`
 	${(props) => {
-		console.log(props);
-
 		if (props.$mt) {
-			console.log('mt');
 			return css`
 				margin: ${(props) => props.$mt + 'px'} 0 0 0;
 			`;
 		} else if (props.$mr) {
-			console.log('mr');
 			return css`
 				margin: 0 ${(props) => props.$mr + 'px'} 0 0;
 			`;
 		} else if (props.$mb) {
-			console.log('mb');
 			return css`
 				margin: 0 0 ${(props) => props.$mb + 'px'} 0;
 			`;
 		} else if (props.$ml) {
-			console.log('ml');
 			return css`
 				margin: 0 0 0 ${(props) => props.$ml + 'px'};
 			`;
 		} else if (props.$mx) {
-			console.log('mx');
 			return css`
 				margin: 0 ${(props) => props.$mx + 'px'};
 			`;
 		} else if (props.$my) {
-			console.log('my');
 			return css`
 				margin: ${(props) => props.$my + 'px'} 0;
 			`;
 		} else if (props.$m) {
-			console.log('$	m');
 			return css`
 				margin: ${(props) => props.$m + 'px'};
 			`;
 		} else {
-			console.log('mttttt');
 			return css`
 			margin: 0};
 			`;
--- src/components/ui/text/text.jsx
+++ src/components/ui/text/text.jsx
@@ -2,22 +2,6 @@ import './text-style.css';
 import './text-styled.js';
 import { StyledText } from './text-styled.js';
 
-// export const TextVariant = {
-// 	normal: 'text',
-// 	bold: 'text text--bold',
-// 	mini: 'text text--small',
-// };
-
-// export const Text = ({
-// 	children,
-// 	variant = TextVariant.normal,
-// 	className = '',
-// }) => {
-// 	return <p className={`${variant} ${className}`}>{children}</p>;
-// };
-
-// ////
-
 const defaultVariantMapping = {
 	h1: 'h1',
 	h2: 'h2',
