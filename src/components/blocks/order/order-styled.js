eec88d410c932809afd8b594e6d5d99a03e6be0e
chackmol <cebep-21@mail.ru>
2025-10-13 12:31:21 +0300

    В блок Order добавлена вторая часть фоновой графики. + графика блока

--- src/components/blocks/order/order-styled.js
+++ src/components/blocks/order/order-styled.js
@@ -1,16 +1,29 @@
 import styled from "styled-components";
-import orderSpotBg from "../../../assests/svg/order-bg-spot.svg";
+import orderBgSpot from "../../../assests/svg/order-bg-spot.svg";
+import orderBgSpotRight from "../../../assests/svg/order-bg-spot-right.svg";
 
 export const StyledOrder = styled.section`
   position: relative;
   background-color: ${(props) => props.theme.colors.colorForLightBackground};
   padding: 100px 70px;
-  min-height: 550px;
+  min-height: 786px;
 
   &:before {
-    content: ${`url(${orderSpotBg})`};
+    content: ${`url(${orderBgSpot})`};
     position: absolute;
     bottom: 0;
     left: 0;
   }
+  &:after {
+    content: ${`url(${orderBgSpotRight})`};
+    position: absolute;
+    top: 175px;
+    right: 74px;
+  }
 `;
+
+export const StyledOrderBox = styled.div`
+  display: flex;
+`;
+
+export const StyledOrderImg = styled.img``;
--- src/components/blocks/order/order.jsx
+++ src/components/blocks/order/order.jsx
@@ -1,5 +1,6 @@
-import { StyledOrder } from "./order-styled";
+import { StyledOrder, StyledOrderBox, StyledOrderImg } from "./order-styled";
 import { Text } from "../../ui/text/text";
+import orderPicture from "../../../assests/svg/order-img.svg";
 
 export const Order = () => {
   return (
@@ -7,6 +8,9 @@ export const Order = () => {
       <Text variant="h1" mb={24}>
         Купить билет
       </Text>
+      <StyledOrderBox>
+        <StyledOrderImg src={orderPicture} alt="кот с цветком" />
+      </StyledOrderBox>
     </StyledOrder>
   );
 };
